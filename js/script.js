ad0e23854a2c3a7ea7c0e803c77ed4f633f4ac80
Guilherme Matias <guimatias890@gmail.com>
2025-09-30 20:21:24 -0300

    feat: novas funcionalidades, evento onclick

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let tarefas = []
+let tarefa = document.querySelector('.input-tarefa')
+
+function adicionarTarefa() {
+    if(tarefas.length){
+       
+    }
+    tarefas.push(tarefa.value) 
+
+}
