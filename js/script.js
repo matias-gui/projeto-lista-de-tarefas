a4122f55c892c140de708945746014c4d36719e4
Guilherme Matias <guimatias890@gmail.com>
2025-10-01 21:17:25 -0300

    style: adicionando eventos

--- js/script.js
+++ js/script.js
@@ -1,10 +1,26 @@
 let tarefas = []
 let tarefa = document.querySelector('.input-tarefa')
+let mensagem = document.querySelector('.mensagem')
+let mostarTarefas = document.querySelector('.mostrar-tarefas')
 
 function adicionarTarefa() {
-    if(tarefas.length){
-       
-    }
-    tarefas.push(tarefa.value) 
+    if(tarefa.value.length === 0){
+       mensagem.style.display = 'block'
+       mensagem.innerHTML = 'Nenhuma tarefa digitada'
+       mensagem.style.animation = 'mensagem  2s ease-in-out ' 
+       setTimeout(()=>{
+        mensagem.style.display = 'none'
+       }, 3000)
+    }else{
+    tarefas.push(tarefa.value)
+    
+    const novaTarefa  = document.createElement ('li')
+    novaTarefa.textContent = tarefa.value
+    mostarTarefas.appendChild(novaTarefa)
+    novaTarefa.style.background = '#ffffffdc'
+    
+
+    tarefa.value = ''
+ }
 
 }
