fa6bdf052b0bd41babc175fc131da4fd558ae4ec
Guilherme Matias <guimatias890@gmail.com>
2025-10-08 21:48:49 -0300

    FEAT: Adicionando butão de exclusão de tarefas

--- js/script.js
+++ js/script.js
@@ -15,12 +15,23 @@ function adicionarTarefa() {
     tarefas.push(tarefa.value)
     
     const novaTarefa  = document.createElement ('li')
-    novaTarefa.textContent = tarefa.value
+    novaTarefa.textContent = tarefa.value 
+
+    const img = document.createElement('img')
+    img.src = 'img /delete_6861362.png'
+    img.alt = 'Excluir'
+    img.style.width = '16px'
+    img.style.cursor = 'pointer'
+    img.onclick = function(){
+      novaTarefa.remove()
+    }
+
+    novaTarefa.appendChild(img)
     mostarTarefas.appendChild(novaTarefa)
-    novaTarefa.style.background = '#ffffffdc'
-    
 
     tarefa.value = ''
- }
-
+    }
+   
 }
+
+
