adbe654fa60e84f90800ab61e2b84f609e115419
Guilherme Matias <guimatias890@gmail.com>
2025-10-11 18:22:15 -0300

    feat : Adicionando a função para editar

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
+
+
 let tarefas = []
 let tarefa = document.querySelector('.input-tarefa')
 let mensagem = document.querySelector('.mensagem')
 let mostarTarefas = document.querySelector('.mostrar-tarefas')
+let ul = document.querySelector('ul')
 
 function adicionarTarefa() {
     if(tarefa.value.length === 0){
@@ -11,27 +14,73 @@ function adicionarTarefa() {
        setTimeout(()=>{
         mensagem.style.display = 'none'
        }, 3000)
+    }else if(tarefa.value.length >= 30){
+       mensagem.innerHTML = 'Texto muito longo '
+       mensagem.style.display = 'block'
+       mensagem.style.animation = 'mensagem  2s ease-in-out ' 
+       setTimeout(()=>{
+        mensagem.style.display = 'none'
+        mensagem.style.animation = ''
+       }, 3000)
     }else{
     tarefas.push(tarefa.value)
     
     const novaTarefa  = document.createElement ('li')
     novaTarefa.textContent = tarefa.value 
 
-    const img = document.createElement('img')
-    img.src = 'img /delete_6861362.png'
-    img.alt = 'Excluir'
-    img.style.width = '16px'
-    img.style.cursor = 'pointer'
-    img.onclick = function(){
+    const img1 = document.createElement('img')
+    img1.src = 'img/delete_24dp_BB271A_FILL0_wght400_GRAD0_opsz24.png'
+    img1.alt = 'Excluir'
+    img1.style.width = '16px' 
+    img1.style.cursor = 'pointer'
+    img1.style.position = 'absolute'
+    img1.style.right = '110px'
+    img1.onclick = function(){
       novaTarefa.remove()
     }
+    const img2 = document.createElement('img')
+    const tarefaEdit = document.createElement('input')
+    const buttonEdit = document.querySelector('.buttonEdit')
+    tarefaEdit.style.display = 'none'
+   
+    img2.src = 'img/edit_24dp_BB271A_FILL0_wght400_GRAD0_opsz24.png'
+    img2.alt = 'Editar'
+    img2.style.width = '16px' 
+    img2.style.cursor = 'pointer'
+    img2.style.position = 'absolute'
+    img2.style.right = '80px'
+    img2.onclick = function(){
+      tarefaEdit.style.display = 'block'
+      tarefaEdit.value = novaTarefa.textContent
+      tarefaEdit.focus()
+      novaTarefa.style.display = 'none'
+      buttonEdit.style.display = 'block'
+      
+      buttonEdit.onclick = function () {
+        novaTarefa.textContent = tarefaEdit.value
+        tarefaEdit.style.display = 'none'
+        buttonEdit.style.display = 'none'
+        novaTarefa.style.display = 'block'
+        img1.style.display = 'block'
+        img2.style.display = 'block'
+       
+      }
+      }
+    
 
-    novaTarefa.appendChild(img)
+    
+
+   
+
+
+    novaTarefa.appendChild(img1)
+    novaTarefa.appendChild(img2)
+    ul.appendChild(tarefaEdit)
+    ul.appendChild(buttonEdit)
     mostarTarefas.appendChild(novaTarefa)
 
     tarefa.value = ''
     }
-   
-}
+  }
 
 
